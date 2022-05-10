89dbb5475a8505bd77d0ecf23571f6fb8030f203
Yeeun Ju <yeju@c1r3s4.42seoul.kr>
2022-05-10 16:41:54 +0900

    key_update

--- result/src/movement.c
+++ result/src/movement.c
@@ -6,7 +6,7 @@
 /*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/05 12:59:50 by yeju              #+#    #+#             */
-/*   Updated: 2022/05/10 13:25:15 by yeju             ###   ########.fr       */
+/*   Updated: 2022/05/10 16:41:19 by yeju             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,11 @@ void	player_move_front(t_info *info)
 
 	mvx = info->pos->dir_x * info->pos->move_speed;
 	mvy = info->pos->dir_y * info->pos->move_speed;
-	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0' ||
-		info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '3')
+	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0')
 	{
 		info->pos->pos_x += mvx;
 	}
-	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0' ||
-		info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '3')
+	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0')
 	{
 		info->pos->pos_y += mvy;
 	}
@@ -55,39 +53,39 @@ void	player_move_back(t_info *info)
 	}
 }
 
-void	player_move_left(t_info *info)
-{
-	double	mvx;
-	double	mvy;
+// void	player_move_left(t_info *info)
+// {
+// 	double	mvx;
+// 	double	mvy;
 
-	mvx = info->pos->plane_x * info->pos->move_speed;
-	mvy = info->pos->plane_y * info->pos->move_speed;
-	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0')
-	{
-		info->pos->pos_x += mvx;
-	}
-	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0')
-	{
-		info->pos->pos_y += mvy;
-	}
-}
+// 	mvx = info->pos->plane_x * info->pos->move_speed;
+// 	mvy = info->pos->plane_y * info->pos->move_speed;
+// 	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0')
+// 	{
+// 		info->pos->pos_x += mvx;
+// 	}
+// 	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0')
+// 	{
+// 		info->pos->pos_y += mvy;
+// 	}
+// }
 
-void	player_move_right(t_info *info)
-{
-	double	mvx;
-	double	mvy;
+// void	player_move_right(t_info *info)
+// {
+// 	double	mvx;
+// 	double	mvy;
 
-	mvx = info->pos->plane_x * info->pos->move_speed * -1;
-	mvy = info->pos->plane_y * info->pos->move_speed * -1;
-	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0')
-	{
-		info->pos->pos_x += mvx;
-	}
-	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0')
-	{
-		info->pos->pos_y += mvy;
-	}
-}
+// 	mvx = info->pos->plane_x * info->pos->move_speed * -1;
+// 	mvy = info->pos->plane_y * info->pos->move_speed * -1;
+// 	if (info->map->world_map[(int)info->pos->pos_y][(int)(info->pos->pos_x + mvx)] == '0')
+// 	{
+// 		info->pos->pos_x += mvx;
+// 	}
+// 	if (info->map->world_map[(int)(info->pos->pos_y + mvy)][(int)info->pos->pos_x] == '0')
+// 	{
+// 		info->pos->pos_y += mvy;
+// 	}
+// }
 
 void	rotate_left(t_info *info)
 {
@@ -120,12 +118,12 @@ int	key_update(t_info *info)
 	/*if (info->key->up, down 함수로 뺌*/
 	if (info->key->w == 1)
 		player_move_front(info);
-	if (info->key->a == 1)
-		player_move_right(info);
+	// if (info->key->a == 1)
+		// player_move_right(info);
 	if (info->key->s == 1)
 		player_move_back(info);
-	if (info->key->d == 1)
-		player_move_left(info);
+	// if (info->key->d == 1)
+		// player_move_left(info);
 	if (info->key->left == 1)
 		rotate_left(info);
 	if (info->key->right == 1)
