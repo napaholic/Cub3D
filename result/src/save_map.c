c82dd4784e5bf703f25d4e3e7f4f93f6cd3c629f
Yeeun Ju <yeju@c1r3s4.42seoul.kr>
2022-05-12 19:39:37 +0900

    주석과 testcode삭제

--- result/src/save_map.c
+++ result/src/save_map.c
@@ -6,13 +6,13 @@
 /*   By: yeju <yeju@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 18:07:07 by yeju              #+#    #+#             */
-/*   Updated: 2022/05/12 19:38:09 by yeju             ###   ########.fr       */
+/*   Updated: 2022/05/12 19:39:18 by yeju             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Cub3D.h"
 
-void	save_map_get_size(char *line_map, t_info *info) //wid, hei는 malloc때 쓰이기 때문에 -1해서 저장됨
+void	save_map_get_size(char *line_map, t_info *info)
 {
 	int	i;
 	int	wid;
@@ -33,10 +33,6 @@ void	save_map_get_size(char *line_map, t_info *info) //wid, hei는 malloc때 쓰
 	i = 0;
 	while (hei--)
 	{
-		//test code
-		// printf(" linemap: \n%s\n", line_map);
-		// printf(" 1: %d\n", wid);
-		// printf(" 2: %d\n", wid2);
 		info->map->map_width = wid2;
 		wid = 0;
 		while (line_map[i++] != '\n')
@@ -44,9 +40,6 @@ void	save_map_get_size(char *line_map, t_info *info) //wid, hei는 malloc때 쓰
 		if (wid2 < wid)
 			wid2 = wid;
 	}
-	//test code
-	// printf("hei: %d\n", info->map->map_height);
-	// printf("wid: %d\n", info->map->map_width);
 	return ;
 }
 
@@ -71,9 +64,6 @@ void	set_pos(char **world_map, t_info *info)
 		}
 		hei++;
 	}
-	//test code
-	printf("posx: %f\n", info->pos->pos_x);
-	printf("posy: %f\n", info->pos->pos_y);
 }
 
 char	**save_map(char *line_map, t_info *info)
@@ -81,9 +71,8 @@ char	**save_map(char *line_map, t_info *info)
 	int	idx;
 
 	idx = 0;
-	free(info->map->world_map); //init할때 했던 malloc 풀기
-	save_map_get_size(line_map, info); //map size 재기
-	//malloc
+	free(info->map->world_map);
+	save_map_get_size(line_map, info);
 	info->map->world_map = (char **)malloc(sizeof(char *) * info->map->map_height);
 	utils_bzero(info->map->world_map, sizeof(char *) * info->map->map_height);
 	while (idx++ < info->map->map_height)
@@ -91,26 +80,18 @@ char	**save_map(char *line_map, t_info *info)
 		info->map->world_map[idx] = (char *)malloc(sizeof(char) * info->map->map_width);
 		utils_bzero(info->map->world_map[idx], sizeof(char) * info->map->map_width);
 	}
-	//malloc한 **map에 map저장
 	info->map->world_map = utils_split(line_map, '\n');
-	//test code
-	// for (int i = 0; i < info->map->map_height + 2; i++)
-	// 	printf("%s ", world_map[i]);
 
-	//set pos
-	set_pos(info->map->world_map, info); //pos도 당연히 인덱스값이라 -1상태
+	set_pos(info->map->world_map, info);
 	if (info->pos->pos_x == -20.0 || info->pos->pos_y == -20.0)
 	{
 		printf("Error\n pos setting");
 		exit(1);
 	}
 
-
-	//map check
+	//map check?
 	// save_map_check(map);
 	
-	//set pos? & 잘못된 pos일 경우
-	info->map->world_map = 0;
 	free(line_map);
 	return (info->map->world_map);
 }
